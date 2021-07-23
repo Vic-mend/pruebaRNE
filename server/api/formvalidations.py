9af923128b691d5b7bf2f43defda416340856712
MiguelPV1 <pvmigue02@gmail.com>
2021-07-23 16:10:00 -0500

    Actualizar, Borrar y validaciones en estacion terrena

--- server/api/formvalidations.py
+++ server/api/formvalidations.py
@@ -15,3 +15,22 @@ def estaciones_validacion(values):
     if(len(values['formato-otro']) > 10 or len(values['formato-otro']) < 1 ): val=False
 
   return val
+
+def login_validation(values):
+  val = True
+  if(len(values['indicativoL']) > 10 or len(values['indicativoL']) < 1 ): val=False
+  if(len(values['contrasenaL']) > 100 or len(values['contrasenaL']) < 1 ): val=False
+
+  return val
+
+def register_validation(values):
+  val = True
+  if(len(values['indicativoR']) > 10 or len(values['indicativoR']) < 1 ): val=False
+  if(len(values['contrasenaR']) > 100 or len(values['contrasenaR']) < 1 ): val=False
+  if(len(values['nombreR']) > 25 or len(values['nombreR']) < 1 ): val=False
+  if(len(values['apellidoPR']) > 25 or len(values['apellidoPR']) < 1 ): val=False
+  if(len(values['apellidoMR']) > 25 or len(values['apellidoMR']) < 1 ): val=False
+  if(len(values['estadoR']) > 30 or len(values['estadoR']) < 1 ): val=False
+  if(len(values['municipioR']) > 100 or len(values['municipioR']) < 1 ): val=False
+
+  return val
